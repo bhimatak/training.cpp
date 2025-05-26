e90d80b7a77e0f584566fd29945993757dd08bdd
bhimatak <bhima.t@gmail.com>
2025-05-26 23:18:06 +0530

    updated

---
+++ classwork/day30/day30/Employee.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <string>
+class Employee
+{
+private:
+	int e_id;
+	char e_name[20];
+};
+
---
+++ classwork/day30/day30/Person.h
@@ -0,0 +1,8 @@
+#pragma once
+class Person
+{
+protected:
+	int phno;
+	char name[20];
+};
+
