e1528f16a903c4cfad2b1c9a587421b64215bc92
bhimatak <bhima.t@gmail.com>
2025-06-10 11:06:31 +0530

    updated

--- classwork/day43/day43/prg02.cpp
+++ classwork/day43/day43/prg02.cpp
@@ -10,19 +10,29 @@ class Employee {
 	int id;
 	string name;
 public:
+	Employee():id(0),name(""){}
 	Employee(int id, string name) : id(id), name(name) {}
+	Employee(Employee& others) {
+		this->id = others.id;
+		this->name = others.name;
+	}
 	void disp() const { cout << "ID: " << id << "\tName: " << name << endl; }
 	int getId() const { return id; }
 	string getName() const { return name; }
 	void setName(string name) { this->name = name; }
 	void setId(int id) { this->id = id; }
 	void setValues() { cin >> id >> name; }
+
+	friend istream &operator >>(istream& is,Employee& e)
+	{
+		is >> e.id >> e.name;
+		return is;
+	}
 };
 
 int main()
 {
-	int id;
-	string name;
+	
 	
 	istream_iterator<Employee> inIt(std::cin);
 	istream_iterator<Employee> endIt;
@@ -37,6 +47,8 @@ int main()
 
 	for (auto e : emp)
 		e.disp();
+
+	
 
 	return 0;
 }
