a80856d9a9b669bbee29c743689e95ffa663fde0
bhimatak <bhima.t@gmail.com>
2025-06-10 14:13:50 +0530

    updated

---
+++ classwork/day43/day43/prg03.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iterator>
+#include <deque>
+#include <algorithm>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+	deque<char> d = { 'A','B','C' };
+	list<int> l = { 1,2,3 };
+	auto it = d.begin();
+
+	cout << it[1] << endl;
+
+	auto itl = l.begin();
+	//cout << itl[1] << endl;
+	cout << *itl << endl;
+	return 0;
+}
